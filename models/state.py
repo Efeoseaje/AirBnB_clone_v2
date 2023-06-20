1eb086a8cc3ab1ccda791af6df8f2043a4be714e
Efeoseaje <efeoseaje@yahoo.com>
2023-06-20 21:28:45 +0100

    DBStorage - States and Cities

--- models/state.py
+++ models/state.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from models.city import City
+import models
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                        backref="state")
+    @property
+    def cities(self):
+        all_cities = models.storage.all(City)
+        return [city for city in all_cities.values()
+                if city.state_id == self.id]
