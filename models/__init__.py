0cdc22477e49dff575ba19bd140b80fe7378520a
Efeoseaje <efeoseaje@yahoo.com>
2023-06-20 22:12:39 +0100

    DBStorage - States and Cities

--- models/__init__.py
+++ models/__init__.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-from models.engine.file_storage import FileStorage
-from AirBnB_clone_v2.models.engine.db_storage import DBStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from os import getenv
-"""__init__ module"""
+"""This module instantiates an object of class FileStorage"""
+import os
 
-classes = {'BaseModel': 'BaseModel', 'Amenity': 'Amenity', 'State': 'State',
-           'Place': 'Place', 'Review': 'Review', 'User': 'User'}
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
+type_storage = os.getenv('HBNB_TYPE_STORAGE')
+
+
+if type_storage == "db":
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
